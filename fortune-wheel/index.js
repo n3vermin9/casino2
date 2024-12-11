002145b40b86ec164ad0e0f80b2a6e2ab5279446
n3vermnin9 <89288662119s@mail.ru>
2024-12-11 18:39:07 +0300

    remake wheel

--- fortune-wheel/index.js
+++ fortune-wheel/index.js
@@ -16,6 +16,8 @@ window.onload = function() {
   if (!balanceValue || balanceValue === null || balanceValue === NaN) {
     localStorage.setItem('balance', 0)
     balance.innerText = balanceValue
+} else {
+    balance.innerText = balanceValue
 }
 };
 
@@ -24,7 +26,7 @@ balance.innerText = balanceValue
 let isStarted = false
 
 btnRotate.addEventListener('click', () => {
-    if (input.value > 0 && !isStarted) {
+    if (input.value > 0 && !isStarted && parseInt(balance.innerText) >= input.value) {
         isStarted = true
         let current = Math.floor(Math.random() * -345)
         current -= 720
@@ -42,44 +44,44 @@ btnRotate.addEventListener('click', () => {
               isStarted = false  
           }, 2500);
             switch (true) {
-                case (current <= 14 && current >= -15 || current <= -196 && current >= -225):
+                case (current <= 14 && current >= -15):
                   modal.innerText = 'money back'
                   localStorage.setItem('balance', balanceValue);
                   balance.innerText = localStorage.getItem('balance')
                     break;
 
-                case (current <= -16 && current >= -45 || current <= -136 && current >= -164):
+                case (current <= -46 && current >= -75):
                     modal.innerText = `you won $${input.value * 2}`
                     localStorage.setItem('balance', parseInt(balanceValue) + parseInt(input.value));
                     localStorage.setItem('balance', input.value * 2 + parseInt(balanceValue));
                     balance.innerText = localStorage.getItem('balance')
                     break;
-
-                case (current <= -46 && current >= -75 || current <= -165 && current >= -195):
-                    modal.innerText = `you won $${input.value * 3}`
-                    localStorage.setItem('balance', parseInt(balanceValue) + parseInt(input.value));
-                    localStorage.setItem('balance', input.value * 2 + parseInt(balanceValue));
-                    balance.innerText = localStorage.getItem('balance')
-                    break;
-
-                case (current <= -76 && current >= -104 || current <= -225 && current >= -255):
+                    
+                case (current <= -105 && current >= -135):
                     modal.innerText = `you won $${input.value * 4}`
                     localStorage.setItem('balance', parseInt(balanceValue) + parseInt(input.value));
                     localStorage.setItem('balance', input.value * 3 + parseInt(balanceValue));
                     balance.innerText = localStorage.getItem('balance')
                     break;  
 
-                case (current <= -105 && current >= -135 || current <= -256 && current >= -285 || current <= -316 && current >= -345):
-                    modal.innerText = 'you lost'
-                    balance.innerText = localStorage.getItem('balance')
-                    break; 
-
-                case (current <= -286 && current >= -315):
+                case (current <= -165 && current >= -195):
                     modal.innerText = `you won $${input.value * 10}`
                     localStorage.setItem('balance', parseInt(balanceValue) + parseInt(input.value));
                     localStorage.setItem('balance', input.value * 9 + parseInt(balanceValue));
                     balance.innerText = localStorage.getItem('balance')
                     break;
+
+                case (current <= -226 && current >= -255 || current <= -165 && current >= -195):
+                    modal.innerText = `you won $${input.value * 3}`
+                    localStorage.setItem('balance', parseInt(balanceValue) + parseInt(input.value));
+                    localStorage.setItem('balance', input.value * 2 + parseInt(balanceValue));
+                    balance.innerText = localStorage.getItem('balance')
+                    break;
+
+                case (current <= -16 && current >= -45 || current <= -76 && current >= -104 || current <= -136 && current >= -164 || current <= -196 && current >= -225 || current <= -256 && current >= -285 || current <= -316 && current >= -345):
+                    modal.innerText = 'you lost'
+                    balance.innerText = localStorage.getItem('balance')
+                    break; 
             }
           wheel.style.transform = `rotate(${0}deg)`;
       }, 4000);
@@ -93,7 +95,7 @@ input.addEventListener("input", function() {
     if (this.value.length > 0 && this.value[0] === '0') {
         this.value = this.value.substring(1);
     }
-    if (this.value > parseInt(balanceValue)) {
-        this.value = balanceValue;
+    if (this.value > parseInt(balance.innerText)) {
+        this.value = balance.innerText;
     }
 });
